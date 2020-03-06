ba08bf4e4437840f0c817114b003003411e05bcc
zamaraevki <zamaraevki@gmail.com>
2020-03-06 11:34:13 +0500

    А существует ли треугольник?[#238095]

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -4,7 +4,7 @@ public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        double rsl = l * h;
+        double rsl = l + h;
         return rsl;
     }
 
--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 public class Triangle {
 
     public static boolean exist(double ab, double ac, double bc) {
-        boolean trian = ab + ac > bc && ab + bc > ac && ac + bc > ab;
-        return trian;
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
 
     }
 }
