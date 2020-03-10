d799dce711111a0b58350d492c1a4b23eed164bc
zamaraevki <zamaraevki@gmail.com>
2020-03-10 20:29:15 +0500

    Простое число.[#238172]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
+
+        }
+
+        return prime;
+    }
+}
