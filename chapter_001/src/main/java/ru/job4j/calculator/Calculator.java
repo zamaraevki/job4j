a4dee85b083797bb57dd480a0d46ee542ccc732d
zamaraevki <zamaraevki@gmail.com>
2020-03-12 18:54:42 +0500

    JavaDoc. Code convention[#238179]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,12 +1,24 @@
 package ru.job4j.calculator;
 
+/**
+ * Сложение 2-х чисел
+ * @author Konstantin Zamaraev (zamaraevki@gmail.com)
+ */
 public class Calculator {
-
+    /**
+     * Method plus.
+     * @param first первое число.
+     * @param second второе число.
+     * @return first plus second.
+     */
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
-
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         Calculator.plus(100, 500);
         Calculator.plus(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/packageinfo.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Konstantin Zamaraev (zamaraevki@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculator;
+
