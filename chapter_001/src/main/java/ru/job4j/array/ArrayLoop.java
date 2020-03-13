bb4b351405b006c066bb3b7501f06eece31255b8
zamaraevki <zamaraevki@gmail.com>
2020-03-13 17:54:18 +0500

    Заполнить массив степенями чисел.[#238098]

--- chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayLoop.java
@@ -5,9 +5,10 @@ public class ArrayLoop {
         int[] numbers = new int[5];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2 + 3;
-        }
-        for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
-        }
+     }
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
     }
 }
