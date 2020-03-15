18ebcc9215f9f04c72e1f37cdebcf2a52d22757b
zamaraevki <zamaraevki@gmail.com>
2020-03-15 13:32:06 +0500

    Поиск индекса в диапазоне.[#238106]. Исправления

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,16 +1,16 @@
 package ru.job4j.array;
 
 public class FindLoop {
-//    public static int indexOf(int[] data, int el) {
-//        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-//        for (int index = 0; index < data.length; index++) {
-//            if (data[index] == el) {
-//                rst = index;
-//                break;
-//            }
-//        }
-//        return rst;
-//    }
+    public static int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
     public static int indexOfstartToFinish(int[] data, int el, int start, int finish) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = start; index <= finish; index++) {
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -6,14 +6,14 @@ import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
 
-//    @Test
-//    public void whenArrayHasLength5Then0() {
-//        int[] input = new int[] {5, 10, 3};
-//        int value = 5;
-//        int result = FindLoop.indexOf(input, value);
-//        int expect = 0;
-//        assertThat(result, is(expect));
-//    }
+    @Test
+    public void whenArrayHasLength5Then0() {
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
 
     @Test
     public void whenFind3() {
