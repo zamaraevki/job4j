0b1341308948e17e2ac8372cc2453c7fee8b974f
zamaraevki <zamaraevki@gmail.com>
2020-05-04 17:07:48 +0500

    2. Перегрузка конструктора.[#238280] исправления

--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -1,6 +1,10 @@
 package ru.job4j.tracker;
 
 public class Predator extends Animal {
+
+    public Predator() {
+    }
+
     public Predator(String name) {
         super(name);
         System.out.println("Predator");
--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,6 +1,10 @@
 package ru.job4j.tracker;
 
 public class Tiger extends Predator {
+
+    public Tiger() {
+    }
+
     public Tiger(String name) {
         super(name);
         System.out.println("Tiger");
